5debf415e28009dc80f2306b5fec50dff965b002
aihao2000 <aihao2000@outlook.com>
2023-11-09 01:05:29 +0800

    add main

--- app.py
+++ app.py
@@ -90,101 +90,99 @@ def style_transfer(prompt, blueprint, num_inference_steps):
     blueprint = color_inversion(blueprint)
     return inference(prompt, blueprint, num_inference_steps)
 
-
-with gr.Blocks() as demo:
-    gr.Markdown(
+if __name__ == "__main__":
+    with gr.Blocks() as demo:
+        gr.Markdown(
+            """
+        # Stable Diffusion Reference Only Automatic Coloring 0.1.2\n\n
+        demo for [https://github.com/aihao2000/stable-diffusion-reference-only](https://github.com/aihao2000/stable-diffusion-reference-only)
         """
-    # Stable Diffusion Reference Only Automatic Coloring 0.1.2\n\n
-    demo for [https://github.com/aihao2000/stable-diffusion-reference-only](https://github.com/aihao2000/stable-diffusion-reference-only)
-    """
-    )
-    with gr.Row():
-        with gr.Column():
-            prompt_input_compoent = gr.Image(shape=(512, 512), label="prompt")
-            prompt_character_segment_button = gr.Button(
-                "character segment",
-            )
-            prompt_character_segment_button.click(
-                character_segment,
-                inputs=prompt_input_compoent,
-                outputs=prompt_input_compoent,
-            )
-        with gr.Column():
-            blueprint_input_compoent = gr.Image(shape=(512, 512), label="blueprint")
-            blueprint_character_segment_button = gr.Button("character segment")
-            blueprint_character_segment_button.click(
-                character_segment,
-                inputs=blueprint_input_compoent,
-                outputs=blueprint_input_compoent,
-            )
-            get_line_art_button = gr.Button(
-                "get line art",
-            )
-            get_line_art_button.click(
-                get_line_art,
-                inputs=blueprint_input_compoent,
-                outputs=blueprint_input_compoent,
-            )
-            color_inversion_button = gr.Button(
-                "color inversion",
-            )
-            color_inversion_button.click(
-                color_inversion,
-                inputs=blueprint_input_compoent,
-                outputs=blueprint_input_compoent,
-            )
-        with gr.Column():
-            result_output_component = gr.Image(shape=(512, 512), label="result")
-            num_inference_steps_input_component = gr.Number(
-                20, label="num inference steps", minimum=1, maximum=1000, step=1
-            )
-            inference_button = gr.Button("inference")
-            inference_button.click(
-                inference,
-                inputs=[
-                    prompt_input_compoent,
-                    blueprint_input_compoent,
-                    num_inference_steps_input_component,
-                ],
-                outputs=result_output_component,
-            )
-            automatic_coloring_button = gr.Button("automatic coloring")
-            automatic_coloring_button.click(
-                automatic_coloring,
-                inputs=[
-                    prompt_input_compoent,
-                    blueprint_input_compoent,
-                    num_inference_steps_input_component,
-                ],
-                outputs=result_output_component,
-            )
-            style_transfer_button = gr.Button("style transfer")
-            style_transfer_button.click(
-                style_transfer,
-                inputs=[
-                    prompt_input_compoent,
-                    blueprint_input_compoent,
-                    num_inference_steps_input_component,
-                ],
-                outputs=result_output_component,
-            )
-    with gr.Row():
-        gr.Examples(
-            examples=[
-                [
-                    os.path.join(
-                        os.path.dirname(__file__), "README.assets", "3x9_prompt.png"
-                    ),
-                    os.path.join(
-                        os.path.dirname(__file__), "README.assets", "3x9_blueprint.png"
-                    ),
-                ],
-            ],
-            inputs=[prompt_input_compoent, blueprint_input_compoent],
-            outputs=result_output_component,
-            fn=lambda x, y: None,
-            cache_examples=True,
         )
-
-
-demo.launch()
+        with gr.Row():
+            with gr.Column():
+                prompt_input_compoent = gr.Image(shape=(512, 512), label="prompt")
+                prompt_character_segment_button = gr.Button(
+                    "character segment",
+                )
+                prompt_character_segment_button.click(
+                    character_segment,
+                    inputs=prompt_input_compoent,
+                    outputs=prompt_input_compoent,
+                )
+            with gr.Column():
+                blueprint_input_compoent = gr.Image(shape=(512, 512), label="blueprint")
+                blueprint_character_segment_button = gr.Button("character segment")
+                blueprint_character_segment_button.click(
+                    character_segment,
+                    inputs=blueprint_input_compoent,
+                    outputs=blueprint_input_compoent,
+                )
+                get_line_art_button = gr.Button(
+                    "get line art",
+                )
+                get_line_art_button.click(
+                    get_line_art,
+                    inputs=blueprint_input_compoent,
+                    outputs=blueprint_input_compoent,
+                )
+                color_inversion_button = gr.Button(
+                    "color inversion",
+                )
+                color_inversion_button.click(
+                    color_inversion,
+                    inputs=blueprint_input_compoent,
+                    outputs=blueprint_input_compoent,
+                )
+            with gr.Column():
+                result_output_component = gr.Image(shape=(512, 512), label="result")
+                num_inference_steps_input_component = gr.Number(
+                    20, label="num inference steps", minimum=1, maximum=1000, step=1
+                )
+                inference_button = gr.Button("inference")
+                inference_button.click(
+                    inference,
+                    inputs=[
+                        prompt_input_compoent,
+                        blueprint_input_compoent,
+                        num_inference_steps_input_component,
+                    ],
+                    outputs=result_output_component,
+                )
+                automatic_coloring_button = gr.Button("automatic coloring")
+                automatic_coloring_button.click(
+                    automatic_coloring,
+                    inputs=[
+                        prompt_input_compoent,
+                        blueprint_input_compoent,
+                        num_inference_steps_input_component,
+                    ],
+                    outputs=result_output_component,
+                )
+                style_transfer_button = gr.Button("style transfer")
+                style_transfer_button.click(
+                    style_transfer,
+                    inputs=[
+                        prompt_input_compoent,
+                        blueprint_input_compoent,
+                        num_inference_steps_input_component,
+                    ],
+                    outputs=result_output_component,
+                )
+        with gr.Row():
+            gr.Examples(
+                examples=[
+                    [
+                        os.path.join(
+                            os.path.dirname(__file__), "README.assets", "3x9_prompt.png"
+                        ),
+                        os.path.join(
+                            os.path.dirname(__file__), "README.assets", "3x9_blueprint.png"
+                        ),
+                    ],
+                ],
+                inputs=[prompt_input_compoent, blueprint_input_compoent],
+                outputs=result_output_component,
+                fn=lambda x, y: None,
+                cache_examples=True,
+            )
+    demo.queue(max_size=10).launch()
