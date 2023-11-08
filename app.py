588f0557c9f54cfabcfe79d3ceb313849894d515
aihao2000 <aihao2000@outlook.com>
2023-11-09 01:07:45 +0800

    remove log device name

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ import os
 import torch
 
 print(f"Is CUDA available: {torch.cuda.is_available()}")
-# True
-print(f"CUDA device: {torch.cuda.get_device_name(torch.cuda.current_device())}")
-# Tesla T4
 if torch.cuda.is_available():
     device = "cuda"
 else:
